7d175e56b6a09295807981c385020a3e760ab910
Andres Orozco <andres.orozco.dev@gmail.com>
2024-05-24 21:36:20 -0400

    fix error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: process.env['URL_BASE_API'] || 'http://localhost:8000/api',
-    apiKey: process.env['SECRET_API_KEY'] || 'development'
+    apiUrl: 'http://localhost:8000/api',
+    apiKey: 'development'
   };
