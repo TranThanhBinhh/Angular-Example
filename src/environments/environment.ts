5150cd21ead21bf4b995a5f70d02396c7e86bd3c
Andres Orozco <andres.orozco.dev@gmail.com>
2024-05-25 17:12:55 -0400

    change url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api',
+    apiUrl: 'https://backend-fastapi-python.azurewebsites.net/api',
     apiKey: 'development'
   };
